1a3ae611aef4219b77154efd9a45292219e71a13
Arvind Hariharan Nair <arvindhn602@gmail.com>
2016-11-14 12:52:05 +0530

    fixed issue with devel package

--- install/devel.sh
+++ install/devel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #development tools
-if [ "$(cat /etc/issue)"=="Arch Linux" ]; then
+if [ "$(cat /etc/issue)" == "Arch Linux" ]; then
   echo "installing devel packages for Arch"
   if test ! $(which ruby); then
     sudo pacman -S --noconfirm ruby
@@ -10,11 +10,15 @@ if [ "$(cat /etc/issue)"=="Arch Linux" ]; then
     sudo pacman -S --noconfirm gcc
   fi
 elif [ -f  /etc/debian_version ]; then
+  echo -e "Running Debain or Debian based distro"
+  echo "installing devel packages"
   sudo apt-get update
   if test ! $(which ruby); then
+    echo "installin ruby"
     sudo apt-get install ruby
   fi
   if test ! $(which gcc); then
+    echo "installing gcc"
     sudo apt-get install gcc
   fi
   # elif [ -f /etc/redhat-release ]; then
