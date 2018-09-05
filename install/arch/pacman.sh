400b21a37d9ff807033124681b61a2117c28e0fe
Arvind Hariharan Nair <arvdev96@gmail.com>
2018-09-05 11:40:43 +0530

    Simplified pacman script

--- install/arch/pacman.sh
+++ install/arch/pacman.sh
@@ -1,50 +1,55 @@
-#if test $(which pacman); then
-#if command -v pacman >/dev/null 2>&1; then
-    
-    if [ $OS = 'Manjaro' ]; then 
-        echo "selecting fastest Mirrors"
-        sudo pacman-mirrors -g
-    fi
+if [ $OS = 'Manjaro' ]; then 
+    echo "selecting fastest Mirrors"
+    sudo pacman-mirrors -g
+fi
 
-    echo "updating packages"
-    sudo pacman -Sy && sudo pacman -Syu
-    
-    if ! command -v yay >/dev/null 2>&1; then
-      git clone https://aur.archlinux.org/yay.git && cd yay
-      makepkg -si --noconfirm
-    fi
+echo "updating packages"
+sudo pacman -Sy && sudo pacman -Syu
 
-    if ! command -v gcc >/dev/null 2>&1; then
-      sudo pacman -S --noconfirm --needed gcc
-    fi
+if ! command -v yay >/dev/null 2>&1; then
+    git clone https://aur.archlinux.org/yay.git $TEMP_DIR && cd $TEMP_DIR/yay
+    makepkg -si --noconfirm
+fi
 
-    if ! command -v ruby >/dev/null 2>&1; then
-      sudo pacman -S --noconfirm --needed ruby
-    fi
+if ! command -v gcc >/dev/null 2>&1; then
+    sudo pacman -S --noconfirm --needed gcc
+fi
 
-    sudo pacman -S --noconfirm --needed binutils \
-    calibre \
-    chromium \
-    flatpak \
-    firefox-developer-edition \
-    curl \
-    gnome-software \
-    gnome-mpv \
-    htop \
-    jdk10-openjdk \
-    openjdk10-doc \
-    nodejs \
-    npm \
-    python-nautilus \
-    vim \
-    stow \
-    tlp \
-    tlp-rdw \
-    tmux \
-    tilix \
-    wget \
-    zsh 
-#fi
+if ! command -v ruby >/dev/null 2>&1; then
+    sudo pacman -S --noconfirm --needed ruby
+fi
+
+sudo pacman -S --noconfirm --needed binutils \
+calibre \
+chromium \
+flatpak \
+firefox \
+firefox-developer-edition \
+curl \
+gnome-software \
+gnome-mpv \
+gufw \
+htop \
+jdk10-openjdk \
+libreoffice-fresh \
+openjdk10-doc \
+nodejs \
+npm \
+python-nautilus \
+openssh \
+p7zip \
+unrar \ 
+rsync \
+steam \
+steam-native-runtime \
+vim \
+stow \
+tlp \
+tlp-rdw \
+tmux \
+tilix \
+wget \
+zsh 
 
 if [ $OS = 'Manjaro']; then
     # remove unnecessary packages
@@ -53,25 +58,11 @@ if [ $OS = 'Manjaro']; then
     empathy \
     mpv
 fi
-
-if [ $OS = 'Arch' ]; then
-    sudo pacman -S --noconfirm --needed firefox \
-    libreoffice-fresh \
-    gufw \
-    openssh \
-    p7zip \
-    unrar \ 
-    rsync \
-    steam \
-    steam-native-runtime \
-    
-    
+# installing aur stuff
+if command -v yay >/dev/null 2>&1; then
     yay -S --noconfirm --needed --aur pamac-aur
-
-    
+    yay -S --noconfirm --needed --aur visual-studio-code-bin
 fi
-
-yay -S --noconfirm --needed --aur visual-studio-code-bin
 source install/common/flatpak.sh
 
 # mv /etc/pacman.d/pacman.conf ~/sysconfback
