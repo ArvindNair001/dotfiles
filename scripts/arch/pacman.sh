308fae65d6fc882d531c97298c23cdfa8508fbe0
Arvind Hariharan Nair <arvdev96@gmail.com>
2018-10-04 01:26:48 +0530

    Add delay

--- scripts/arch/pacman.sh
+++ scripts/arch/pacman.sh
@@ -1,3 +1,5 @@
+#!/usr/bin
+
 if [ $OS = 'Manjaro' ]; then 
     echo "selecting fastest Mirrors"
     sudo pacman-mirrors -g
@@ -5,28 +7,31 @@ fi
 
 echo "updating packages"
 sudo pacman -Sy && sudo pacman -Syu
+sleep 1.0
 
 sudo pacman -S --noconfirm --needed binutils \
 calibre \
+curl \
 chromium \
 flatpak \
 firefox \
 firefox-developer-edition \
-curl \
+fzf \
 gnome-software \
 gnome-mpv \
 gufw \
 htop \
 jdk10-openjdk \
+kvantum-qt5 \
 libreoffice-fresh \
-openjdk10-doc \
 nodejs \
 npm \
-python-nautilus \
+openjdk10-doc \
 openssh \
+python-nautilus \
 p7zip \
-unrar \
 rsync \
+unrar \
 steam \
 steam-native-runtime \
 vim \
@@ -57,11 +62,13 @@ fi
 if ! command -v yay >/dev/null 2>&1; then
     git clone https://aur.archlinux.org/yay.git $TEMP_DIR/yay && cd $TEMP_DIR/yay
     makepkg -si --noconfirm --needed
+    sleep 1.0
 fi
 
 if command -v yay >/dev/null 2>&1; then
     # installing aur stuff
     yay -S --noconfirm --needed --aur pamac-aur
+    sleep 1.0
     yay -S --noconfirm --needed --aur visual-studio-code-bin
 fi
 # mv /etc/pacman.d/pacman.conf ~/sysconfback
