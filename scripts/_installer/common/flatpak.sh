8df080c4910c38b8959b9fe7ad049ad6cc60abbe
Arvind Hariharan Nair <arvdev96@gmail.com>
2018-11-17 14:54:12 +0530

    Removing Nextcloud From flatpak
    Moving from Flatpak to Appimage for Nextlcoud

--- scripts/_installer/common/flatpak.sh
+++ scripts/_installer/common/flatpak.sh
@@ -1,13 +1,13 @@
 if command -v  flatpak >/dev/null 2>&1; then
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak install -y flathub org.nextcloud.Nextcloud \
-    net.ankiweb.Anki \
+    flatpak install -y flathub org.telegram.desktop \
     com.axosoft.GitKraken \
-    org.telegram.desktop \
-    com.github.fabiocolacio.marker \
     io.github.mmstick.FontFinder \
     com.googleplaymusicdesktopplayer.GPMDP \
-    org.gottcode.FocusWriter 
+    org.gottcode.FocusWriter
+    
+    #net.ankiweb.Anki
+    #org.nextcloud.Nextcloud \
 
     #if [ "$OS" == "arch" ]; then
         #flatpak install flathub com.visualstudio.code
