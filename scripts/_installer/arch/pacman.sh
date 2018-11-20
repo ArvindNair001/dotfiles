da4fd8a4d12977d80f4610ef15cd07cf8855ced8
Arvind Hariharan Nair <arvdev96@gmail.com>
2018-11-20 14:42:55 +0530

    Add More KDE apps

--- scripts/_installer/arch/pacman.sh
+++ scripts/_installer/arch/pacman.sh
@@ -11,42 +11,45 @@ sleep 1.0
 
 echo "installing libraries"
 sudo pacman -S --noconfirm --needed binutils \
+cups \
 curl \
 flatpak \
+fzf \
 gcc \
+gvim \
 htop \
+jdk-openjdk \
 libmtp \
-mtpfs\
+mtpfs \
+networkmanager-openvpn \
 nodejs \
 npm \
-networkmanager-openvpn \
 openssh \
-jdk-openjdk \
 openjdk-doc \
-fzf \
 p7zip \
-unrar \
+python-pip \
 rsync \
 ruby \
 stow \
+tmux \
 tlp \
 tlp-rdw \
-gvim \
-tmux \
 ufw \
+unrar \
+unzip \
 wget \
 zsh 
 
 sleep 1.0
 
 echo -e "installing extras"
-sudo pacman -S adobe-source-sans-pro-fonts \
+sudo pacman -S --noconfirm --needed adobe-source-sans-pro-fonts \
 aspell-en \
 enchant \
 gst-libav \
 gst-plugins-good \
 jre8-openjdk \
-hunspell-en \
+hunspell \
 icedtea-web \
 languagetool \
 libmythes \
@@ -67,13 +70,14 @@ firefox-developer-edition \
 gimp \
 libreoffice-fresh \
 steam \
-steam-native-runtime 
-qbittorent \
+steam-native-runtime \
+qbittorrent \
 
 sleep 1.0
 
 # Gnome specific packages
-if [ $XDG_SESSION_DESKTOP == 'KDE' ]; then
+if [ $XDG_SESSION_DESKTOP = 'gnome' ]; then
+    echo "installing Gnome packages"
     sudo pacman -S --noconfirm --needed gnome-software \
     gnome-mpv \
     gnome-tweaks\
@@ -83,24 +87,30 @@ if [ $XDG_SESSION_DESKTOP == 'KDE' ]; then
 fi
 
 # KDE Specific apps
-if [ $XDG_SESSION_DESKTOP == 'KDE' ]; then
+if [ $XDG_SESSION_DESKTOP = 'KDE' ]; then
+    echo "installing KDE packages"
     sudo pacman -S --noconfirm --needed ark \
-    amarok \
     dolphin \
     dolphin-plugins \
     gwenview \
-    kde-cli-tools
+    kde-cli-tools \
+    kdeutils \
     kinfocenter \
     konsole \
-    kwalletmanager \
     kipi-plugins \
     okular \
     plasma-pa \
-    redshift \
     plasma5-applets-redshift-control \
+    redshift \
     spectacle \
     speedcrunch \
     vlc
+
+    sleep 1.5
+
+    pacman -Rns kbackup \
+    kfloppy \
+    ktimer 
 fi
 
 if [ $OS = 'Manjaro' ]; then
@@ -119,7 +129,6 @@ fi
 
 if command -v yay >/dev/null 2>&1; then
     # installing aur stuff 
-    # if desktop is kde
     if [ $XDG_SESSION_DESKTOP == 'KDE' ]; then
         yay -S --noconfirm --needed --aur pamac-aur pamac-tray-appindicator
     else 
